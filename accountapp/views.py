e9dc6cede4f26a80f2ad9163c7c8cd87f42ab2ed
seunghyo <tmdgy6232@naver.com>
2021-07-22 10:16:23 +0900

    7/22 custom decorator 추가 및 method_decorator 리스트로 적용

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
@@ -38,8 +39,10 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-@method_decorator(login_required, 'get') # 클래스에는 원래 decorator를 적용할 수 없지만 method_decorator가 가능하게 해줌
-@method_decorator(login_required, 'post')
+has_ownership = [login_required, account_ownership_required]
+
+@method_decorator(has_ownership, 'get') # 메서드에는 원래 decorator를 적용할 수 없지만 method_decorator가 가능하게 해줌
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -47,8 +50,8 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get') # 메서드에는 원래 decorator를 적용할 수 없지만 method_decorator가 가능하게 해줌
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
