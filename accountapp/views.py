03dc0d2bea73953124d44f280c80d6f88b62e4e1
seunghyo <tmdgy6232@naver.com>
2021-07-22 09:42:39 +0900

    7/22 계정인증부분 decorator 사용하여 적용

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponseForbidden
@@ -5,26 +6,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
-
+@login_required #(login_url=reverse_lazy('accountapp:create')) 로그인 url 디폴트값이 아닐 시에 수정
 def helloworld(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
-            helloWorldInstance = HelloWorld()
-            helloWorldInstance.text = temp
-            helloWorldInstance.save()
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        helloWorldInstance = HelloWorld()
+        helloWorldInstance.text = temp
+        helloWorldInstance.save()
 
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
@@ -39,7 +38,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-
+@method_decorator(login_required, 'get') # 클래스에는 원래 decorator를 적용할 수 없지만 method_decorator가 가능하게 해줌
+@method_decorator(login_required, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -47,33 +47,10 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
-
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
--- decorators.py
+++ decorators.py
@@ -1,24 +1,35 @@
-def decorator1(fucc):
-    def decorator(*args):
+class User:
+    def __init__(self, is_authenticated):
+        self.is_authenticated = is_authenticated
+
+def decorator1(func):
+    def decorator(*args, **kwargs):
         w = args[0]
         h = args[1]
         if w > 0 or h > 0:
             print('good')
-            fucc(*args)
+            return func(*args)
         else:
             raise ValueError('음수다인마')
 
     return decorator
 
+def login_required(func):
+    def decorated(w, h, **kwargs):
+        if kwargs.get('user').is_authenticated:
+            return func(w, h, **kwargs)
+        else: raise ValueError('is_authenticated Error')
+    return decorated
+
+@login_required
 @decorator1
-def triangle(w, h):
+def triangle(w, h, **kwargs):
     val = w * h / 2
-    print(val)
+    return val
 
 @decorator1
-def box(w, h):
+def box(w, h, **kwargs):
     val = w * h
-    print(val)
+    return val
 
-triangle(-22, -22)
-box(3,3)
+print(box(2,3))
