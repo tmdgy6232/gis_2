3a00d367d99423f36b1c9764e2ff5efa30d5ba64
seunghyo <tmdgy6232@naver.com>
2021-07-26 09:44:21 +0900

    7/26 profile create model

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') # 삭제정책 담당하는 on_delete
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    message = models.CharField(max_length=200, nul l=True)
+
+
