52ec3e31eb183a81184c39dd9bb1cf7391d17a77
tmdgy6232 <tmdgy6232@naver.com>
2021-07-29 10:39:41 +0900

    7/27 articleapp 추가

--- gisweb_2/urls.py
+++ gisweb_2/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', helloworld, name='root'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
-    path('profiles/', include('profileapp.urls'))
+    path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # 미디어 파일을 제공받기 위해 추가
 
