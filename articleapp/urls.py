6a47fe35e6e3e6ef4ee4fc5fcb4004436d3e1388
tmdgy6232 <tmdgy6232@naver.com>
2021-08-02 09:19:27 +0900

    8/2 articleapp List 적용 magicgrid 사용하여 레이아웃 적용

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,4 +1,8 @@
-urlpatterns = [
+from django.urls import path, include
+from django.views.generic import TemplateView
 
+#templateView = 로직없이 어떤 html을 쓸건지 말해주면 템플릿만 보여주는 기능을 함
+urlpatterns = [
+    path('list', TemplateView.as_view(template_name='articleapp/list.html'),name='list')
 ]
 
