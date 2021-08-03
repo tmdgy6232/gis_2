7444949bf4224151605d7cc48b31716a0f0454a3
tmdgy6232 <tmdgy6232@naver.com>
2021-08-03 14:56:26 +0900

    8/3 article create 기능 추가

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from articleapp.views import ArticleCreateView
+
+app_name = 'articleapp'
 
 #templateView = 로직없이 어떤 html을 쓸건지 말해주면 템플릿만 보여주는 기능을 함
+
 urlpatterns = [
-    path('list', TemplateView.as_view(template_name='articleapp/list.html'),name='list')
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'),name='list'),
+    path('create/', ArticleCreateView.as_view() ,name='create'),
 ]
 
