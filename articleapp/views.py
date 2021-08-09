255d0cec8b483da142ad2ccf50ec3a8993443630
tmdgy6232 <tmdgy6232@naver.com>
2021-08-09 10:34:30 +0900

    8/9 comment CreateView 추가 및 articleDetail에 commentForm 추가

--- articleapp/views.py
+++ articleapp/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
+from django.views.generic.edit import FormMixin
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
+from commentapp.forms import CommentCreationForm
 
 
 @method_decorator(login_required, 'get')
@@ -26,10 +28,11 @@ class ArticleCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView, FormMixin):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+    form_class = CommentCreationForm
 
 @method_decorator(article_ownership_required, 'get')
 @method_decorator(article_ownership_required, 'post')
