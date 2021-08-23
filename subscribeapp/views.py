d36d059bbd878aa4dbccb18b8125f3878692d58e
tmdgy6232 <tmdgy6232@naver.com>
2021-08-23 09:50:44 +0900

    8/23 구독기능 및 버튼 추가

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
 
-# Create your views here.
+from projectapp.models import Project
+from subscribeapp.models import Subscription
+
+
+@method_decorator(login_required, 'get')
+class SubscriptionView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        project = Project.objects.get(pk=kwargs['project_pk'])
+
+        subscription = Subscription.objects.filter(user=user,
+                                                   project=project)
+
+        if subscription.exists():
+            subscription.delete()
+        else:
+            Subscription(user=user, project=project).save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
