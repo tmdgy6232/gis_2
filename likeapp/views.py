4ecfecacd403d0ddc6633127ea0a9d0c0b33de91
tmdgy6232 <tmdgy6232@naver.com>
2021-08-26 10:51:42 +0900

    8/26 좋아요 기능 transaction 기능 구현

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -12,6 +14,22 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+
+    article.like += 1
+    article.save()
+
+    like_record = LikeRecord.objects.filter(user=user, article=article)
+
+    if like_record.exists():
+        raise ValidationError('like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
 
@@ -19,19 +37,15 @@ class LikeArticleView(RedirectView):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
 
-        like_record = LikeRecord.objects.filter(user=user, article=article)
-
-        if like_record.exists():
-            #좋아요가 이미 있을 시
+        try:
+            db_transaction(user, article)
+            # 좋아요 반영 O
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except:
+            # 좋아요 반영 X
             messages.add_message(request, messages.ERROR, '좋아요는 한 번만 가능합니다.')
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
 
-        article.like += 1
-        article.save()
-        #좋아요 반영 O
-        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
