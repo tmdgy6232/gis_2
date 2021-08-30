0580641c0c73d3d123e0bd3e30b60ae5d5f229fd
tmdgy6232 <tmdgy6232@naver.com>
2021-08-30 09:43:58 +0900

    8/30 hello_world 관련 코드 삭제 및 수정

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -21,7 +21,6 @@ from accountapp.views import helloworld, AccountCreateView, AccountDetailView, A
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', helloworld, name = 'hello_world'),
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'),
--- accountapp/views.py
+++ accountapp/views.py
@@ -12,28 +12,27 @@ from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
-from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 
-@login_required #(login_url=reverse_lazy('accountapp:create')) 로그인 url 디폴트값이 아닐 시에 수정
-def helloworld(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
-        helloWorldInstance = HelloWorld()
-        helloWorldInstance.text = temp
-        helloWorldInstance.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
+# @login_required #(login_url=reverse_lazy('accountapp:create')) 로그인 url 디폴트값이 아닐 시에 수정
+# def helloworld(request):
+#     if request.method == "POST":
+#         temp = request.POST.get('hello_world_input')
+#         helloWorldInstance = HelloWorld()
+#         helloWorldInstance.text = temp
+#         helloWorldInstance.save()
+#
+#         return HttpResponseRedirect(reverse('accountapp:hello_world'))
+#     else:
+#         hello_world_list = HelloWorld.objects.all()
+#         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+#
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 
@@ -65,5 +64,5 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
